7333b4d9fbcd6adcc93f8f83817ea36cb7bdf96a
Felix Dittrich <Felix.Dittrich@t-systems.com>
2021-06-08 18:54:44 +0200

    Update License Header

--- src/main/java/app/coronawarn/dcc/service/LabIdClaimService.java
+++ src/main/java/app/coronawarn/dcc/service/LabIdClaimService.java
@@ -1,3 +1,23 @@
+/*-
+ * ---license-start
+ * Corona-Warn-App / cwa-dcc
+ * ---
+ * Copyright (C) 2020 - 2021 T-Systems International GmbH and all other contributors
+ * ---
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ---license-end
+ */
+
 package app.coronawarn.dcc.service;
 
 import app.coronawarn.dcc.config.DccApplicationConfig;
@@ -16,14 +36,6 @@ public class LabIdClaimService {
   private final LabIdClaimRepository labIdClaimRepository;
 
   private final DccApplicationConfig config;
-
-  /**
-   * Checks if Claim already exists. If not creates a new one if quota is not already exceeded.
-   *
-   * @param partnerId the partner ID
-   * @param labId     the lab ID
-   * @return true if claiming was successful, false if not.
-   */
   public boolean getClaim(String partnerId, String labId) {
     log.debug("Trying to get claim for partnerId {} and labId {}", partnerId, labId);
 
